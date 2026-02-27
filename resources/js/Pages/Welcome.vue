8899ef014eb5d4fdd1afe8ac000751e51d826939
Sharif Khan <455120+TheShakyCoder@users.noreply.github.com>
2026-02-27 22:24:34 +0000

    Add Bomberman link to games list

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -39,6 +39,7 @@ defineProps({
                 <div><a class="underline underline-offset-4" href="/dodge">Dodge</a> (first attempt)</div>
                 <div><a class="underline underline-offset-4" href="/asteroids">Asteroids</a> (my take on an old classic)</div>
                 <div><a class="underline underline-offset-4" href="/battle-zone">BattleZone</a> (a restomod)</div>
+                <div><a class="underline underline-offset-4" href="https://bomber-birds.on-forge.com/" target="_blank">Bomberman</a> (a restomod)</div>
 <!--                <div><a class="underline underline-offset-4" href="https://earth2900.sharifkhan.co.uk/">Earth 2900 (BETA)</a> (homage to Deuteros)</div>-->
 <!--                <div><a class="underline underline-offset-4" href="/lander">Lunar Lander</a> (desktop only, no touchscreen)</div>-->
             </div>
