4b42307ec52f8e6f62f15dc7d420525da95d6328
Sharif Khan <455120+TheShakyCoder@users.noreply.github.com>
2026-03-20 11:25:07 +0000

    Fix link formatting for BackWhen project

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -40,7 +40,7 @@ defineProps({
                 <div><a class="underline underline-offset-4" href="/asteroids">Asteroids</a> (my take on an old classic)</div>
                 <div><a class="underline underline-offset-4" href="/battle-zone">BattleZone</a> (a restomod)</div>
                 <div><a class="underline underline-offset-4" href="https://bomber-birds.sharifkhan.co.uk" target="_blank">BomberBirds</a> (a restomod)</div>
-                <div><a class="underline underline-offset-4" href="https://backwhen.co.uk" target="_blank">BackWhen (private video sharing)</div>
+                <div><a class="underline underline-offset-4" href="https://backwhen.co.uk" target="_blank">BackWhen</a> (private video sharing)</div>
 <!--                <div><a class="underline underline-offset-4" href="https://earth2900.sharifkhan.co.uk/">Earth 2900 (BETA)</a> (homage to Deuteros)</div>-->
 <!--                <div><a class="underline underline-offset-4" href="/lander">Lunar Lander</a> (desktop only, no touchscreen)</div>-->
             </div>
